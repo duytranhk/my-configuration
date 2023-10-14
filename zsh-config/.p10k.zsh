30cec4df81a0677c8408b4e3a9bf379b3ed2d910
Duy Tran <khanhduyth@gmail.com>
2023-10-14 15:50:55 +1000

    update main icon

--- zsh-config/.p10k.zsh
+++ zsh-config/.p10k.zsh
@@ -185,7 +185,7 @@
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=255
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=67
   # Custom icon.
-  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='ﴂ'
+  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='󰱵'
 
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
